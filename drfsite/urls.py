9ccf4de1b77fce65e60b7262742fca64e5a838bf
Pavlov Danil <danpavkzn@mail.ru>
2024-09-29 05:00:57 +0300

    5 finished

--- drfsite/urls.py
+++ drfsite/urls.py
@@ -5,4 +5,5 @@ from news.views import NewsAPIView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/newslist/', NewsAPIView.as_view()),
+    path('api/v1/newslist/<int:pk>/', NewsAPIView.as_view()),
 ]
--- news/views.py
+++ news/views.py
@@ -1,8 +1,46 @@
-from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.views import APIView
 from news.serializers import NewsSerializer
 from .models import *
 
 
-class NewsAPIView(generics.ListAPIView):
-    queryset = News.objects.all()
-    serializer_class = NewsSerializer
+class NewsAPIView(APIView):
+    def get(self, request):
+        n = News.objects.all()
+        return Response({'news': NewsSerializer(n, many=True).data})
+
+    def post(self, request):
+        serializer = NewsSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({'news': serializer.data})
+
+    def put(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'method put not allowed'})
+
+        try:
+            instance = News.objects.get(pk=pk)
+        except:
+            return Response({'error': 'object does not exist'})
+
+        serializer = NewsSerializer(data=request.data, instance=instance)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+
+        return Response({'news': serializer.data})
+
+    def delete(self, request, *args, **kwargs):
+        pk = kwargs.get('pk', None)
+        if not pk:
+            return Response({'error': 'method delete not allowed'})
+
+        try:
+            instance = News.objects.get(pk=pk)
+            instance.delete()
+        except:
+            return Response({'error': 'object does not exist'})
+
+        return Response({"post": "delete post " + str(pk)})
